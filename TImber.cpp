917992ec9e845ca164a7b068c6ea9363e90136be
Stefan-23 <sdrmanic@gmail.com>
2022-08-22 22:43:17 +0200

    Created Tree, Cloud and Bee

--- TImber.cpp
+++ TImber.cpp
@@ -9,10 +9,13 @@ int main()
     //Creating open window for game RenderWindow
     RenderWindow window(vm, "Timber!!", Style::Fullscreen);
 
+
     /*
-        ******** Drawing scene *********
+        Background
+        ********************************************************
     */
-    //Creating texture who holds graphics in GPU
+
+    //Creating texture which holds graphics in GPU
     Texture textureBackground;
 
     //Loading graphics in texture
@@ -27,11 +30,133 @@ int main()
     //Setting background postion so he could cover whole background
     spriteBackground.setPosition(0, 0);
 
+    /*
+        Background-END
+       ********************************************************
+   */
+  
 
+
+   /*
+    Creating Tree
+       ********************************************************
+   */
+
+    Texture textureTree;
+
+    textureTree.loadFromFile("graphics/tree.png");
+
+    Sprite spriteTree;
+
+    spriteTree.setTexture(textureTree);
+
+    spriteTree.setPosition(810, 0);
+
+    /*
+       Tree END
+       ********************************************************
+   */
+
+
+
+   /*
+        Creating BEE 
+       ********************************************************
+   */
+    
+    Texture textureBee;
+
+    textureBee.loadFromFile("graphics/bee.png");
+
+    Sprite spriteBee;
+
+    spriteBee.setTexture(textureBee);
+
+    spriteBee.setPosition(0, 800);
+
+    //Is Bee moving
+    bool isBeeActive = false;
+    //Bee moving speed
+    float beeSpeed = 0.0f;
+
+  
+
+
+    /*
+        BEE end
+       ********************************************************
+   */
+
+
+
+   /*
+       Creating CLOUD
+      ********************************************************
+  */
+
+    Texture textureCloud;
+
+    textureCloud.loadFromFile("graphics/cloud.png");
+
+    //3 new sprite with same texture
+    Sprite spriteCloud1;
+    Sprite spriteCloud2;
+    Sprite spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    //Position
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    //Is it active on window
+    bool isCloudActive1 = false;
+    bool isCloudActive2 = false;
+    bool isCloudActive3 = false;
+
+    //Cloud speed
+    float CloudSpeed1 = 0.0f;
+    float CloudSpeed2 = 0.0f;
+    float CloudSpeed3 = 0.0f;
+
+    /*
+         CLOUD END
+         ********************************************************
+     */
+
+    window.clear();
+
+    /*
+         Drawing scene
+         ********************************************************
+     */
+
+    //Background
     window.draw(spriteBackground);
-    //Showing everyhting we drawed 
-    window.display();
 
+    //3 clouds
+    window.draw(spriteCloud1);
+    window.draw(spriteCloud2);
+    window.draw(spriteCloud3);
+    
+    //Tree
+    window.draw(spriteTree);
+
+    //Bee
+    window.draw(spriteBee);
+
+    window.display();
+    
+    /*
+         Drawing scene
+         ********************************************************
+     */
+    
+    
+    
     //Telling window to close with Esc button
     while (window.isOpen())
     {
@@ -40,11 +165,8 @@ int main()
             window.close();
         }
     }
-    /*
-        Drawing scene
-    */
-    //clearing everything from previous scene
-    window.clear();
+    
+    
 
     
     return 0;
