2ca9bb57b2f5d79b850896be03a4c7a8fb5d9b4d
Stefan-23 <sdrmanic@gmail.com>
2025-11-14 22:08:31 +0100

    Hi, different solution I found out to be working. Please take a look. I couldn't make first solution to work but this works as i should.

---
+++ include/PlayerActions.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+enum class side { LEFT, RIGHT, NONE };
+
+void handleChopRight(
+    side& playerSide,
+    int& score,
+    float& timeRemaining,
+    bool& acceptInput,
+    sf::Sprite& spritePlayerRight,
+    sf::Sprite& spriteHitRight,
+    sf::Sprite& spriteLog,
+    bool& logActive,
+    float& logSpeedX,
+    sf::Sound& chop,
+    sf::Sound& chop2,
+    sf::Sound& chop3,
+    sf::Sound& chop4
+);
+
+void handleChopLeft(
+    side& playerSide,
+    int& score,
+    float& timeRemaining,
+    bool& acceptInput,
+    sf::Sprite& spritePlayerLeft,
+    sf::Sprite& spriteHitLeft,
+    sf::Sprite& spriteLog,
+    bool& logActive,
+    float& logSpeedX,
+    sf::Sound& chop,
+    sf::Sound& chop2,
+    sf::Sound& chop3,
+    sf::Sound& chop4
+);
---
+++ src/PlayerActions.cpp
@@ -0,0 +1,91 @@
+#include "../include/PlayerActions.h"
+#include <cstdlib>
+
+void updateBranches(int seed);
+
+
+const float HIT_POSITION_LEFT = 700.f;
+const float HIT_POSITION_RIGHT = 1075.f;
+
+void handleChopRight(
+    side& playerSide,
+    int& score,
+    float& timeRemaining,
+    bool& acceptInput,
+    sf::Sprite& spritePlayerRight,
+    sf::Sprite& spriteHitRight,
+    sf::Sprite& spriteLog,
+    bool& logActive,
+    float& logSpeedX,
+    sf::Sound& chop,
+    sf::Sound& chop2,
+    sf::Sound& chop3,
+    sf::Sound& chop4
+)
+{
+    playerSide = side::RIGHT;
+    spritePlayerRight.setPosition(1100, 850);
+    score++;
+    timeRemaining += (2.f / score) + .15f;
+
+    spriteHitRight.setPosition(HIT_POSITION_RIGHT, spriteHitRight.getPosition().y);
+
+    updateBranches(score);
+
+    spriteLog.setPosition(810, 900);
+    logSpeedX = -5000.f;
+    logActive = true;
+    acceptInput = false;
+
+    chop.play();
+
+    int randFuncIdx = std::rand() % 4;
+    switch (randFuncIdx) {
+    case 0: chop2.play(); break;
+    case 1: chop3.play(); break;
+    case 2: chop4.play(); break;
+    default: break;
+    }
+}
+
+void handleChopLeft(
+    side& playerSide,
+    int& score,
+    float& timeRemaining,
+    bool& acceptInput,
+    sf::Sprite& spritePlayerLeft,
+    sf::Sprite& spriteHitLeft,
+    sf::Sprite& spriteLog,
+    bool& logActive,
+    float& logSpeedX,
+    sf::Sound& chop,
+    sf::Sound& chop2,
+    sf::Sound& chop3,
+    sf::Sound& chop4
+)
+{
+    playerSide = side::LEFT;
+    score++;
+    timeRemaining += (2.f / score) + .15f;
+
+    spriteHitLeft.setPosition(HIT_POSITION_LEFT, spriteHitLeft.getPosition().y);
+    spritePlayerLeft.setPosition(650, 850);
+
+    updateBranches(score);
+
+    spriteLog.setPosition(810, 900);
+    logSpeedX = 5000.f;
+    logActive = true;
+    acceptInput = false;
+
+    chop.play();
+
+    int randFuncIdx = std::rand() % 4;
+    switch (randFuncIdx) {
+    case 0: chop2.play(); break;
+    case 1: chop3.play(); break;
+    case 2: chop4.play(); break;
+    default: break;
+    }
+
+}
